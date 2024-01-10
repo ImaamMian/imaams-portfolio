845c0104e16bb00dbe366e5a3d1331b282ec3fd1
imia307 <imia307@aucklanduni.ac.nz>
2024-01-11 00:34:06 +1300

    added personal information to a js
    added my own information to a js file and will change it in the future so you can see these details on the website

---
+++ portfolio/src/data/constants.js
@@ -0,0 +1,154 @@
+export const Bio = {
+  name: "Imaam Mian",
+  roles: [
+    "Full Stack Developer",
+    "Data Analyst",
+    "UI/UX Designer",
+    "Programmer",
+  ],
+  description:
+    "I am an enthusiastic and adaptable individual, constantly seeking opportunities to broaden my skills. Fueled by a strong desire for knowledge, I am committed to producing excellent outcomes. Embracing a positive outlook and possessing a mindset geared towards growth, I am prepared to bring valuable contributions and attain significant accomplishments.",
+  github: "https://github.com/ImaamMian",
+  resume:
+    "https://drive.google.com/file/d/1gm2iEuZEHTaxUAplK2QqvAzSfbXLm1am/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/imaam-mian-0754071ba/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:"https://ajeetchaulagain.com/static/7cb4af597964b0911fe71cb2f8148d64/87351/express-js.png",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "ASP.NET",
+        image: "https://upload.wikimedia.org/wikipedia/commons/e/ee/.NET_Core_Logo.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Vercel",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/5/5e/Vercel_logo_black.svg",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://wickedhots.co.nz/cdn/shop/files/WICKED-HOTS-OUTLINED.png?v=1695362722&width=600",
+    role: "Casual Picker and Packer",
+    company: "Wicked Hots",
+    date: "Dec 2022 - Feb 2023",
+    desc: "Operated automated machinery in the packing section, overseeing the labelling and sealing processes for chillies. Executed proficient packing procedures in preparation for shipment and retail distribution. \n"+ 
+    "Engaged in diverse responsibilities within the greenhouse environment, specialising in the harvesting of"+
+    "chillies and undertaking supplementary tasks such as weed spraying. Proficiently facilitated the transfer of"+
+    "harvested produce from the greenhouse to the packing house.",
+    skills: [
+      "Attention to detail",
+      "Communication",
+      "TeamWork",
+      "Time Management",
+    ],
+  },
+  {
+    id: 1,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/neurobit.jpeg?alt=media&token=1cacbb7b-e5ea-4efb-9b3d-f5a31b9564ad",
+    role: "Fabrication Worker",
+    company: "Dynex LTD",
+    date: "Dec 2021 - Feb 2022",
+    desc: "Analysing stock availability to optimise inventory management \n"+ "Efficiently managing labelling, sorting and sending of packages to be delivered in a timely manner \n" +
+    "Proficiently completed various paperwork in an accurate and efficient manner. \n"+
+    "Playing a crucial role in processing products while maintaining high standards through rigorous quality control measures, following complex instructions to a tee.",
+    skills: [
+      "Quality control",
+      "Problem Solving",
+      "Communication",
+    ],
+  },
+];
+
